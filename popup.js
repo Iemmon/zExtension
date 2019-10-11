59eec7c7bd5d6b2321e2b84a92b4ada2a2a93ad0
Iemmon <katerynab.ko@gmail.com>
2019-10-11 19:48:23 +0300

    XMLHttpRequest added

--- popup.js
+++ popup.js
@@ -10,31 +10,43 @@ chrome.storage.sync.get('color', function(data) {
   changeColor.setAttribute('value', data.color);
 });
 */
-chrome.storage.sync.get({'series':[]}, function({series: result}){
+chrome.storage.sync.get({ 'series': [] }, function ({ series: result }) {
   console.log(result);
-  if(result != null){
-    for(let el of result){
-      
-    var tableElementTr = document.createElement("TR");
 
-    var textnodeNameTd = document.createElement("td");
-    // textnodeNameTd.innerText = el.name;
+    for (let el of result) {
 
-    const linkElement = document.createElement("a");
-    linkElement.href = '#';
-    linkElement.innerText = el.name;
-    linkElement.onclick = function() {
-      chrome.tabs.create({active: true, url: el.link});
+      const tableElementTr = document.createElement("TR");
+
+      const textnodeNameTd = document.createElement("td");
+      // textnodeNameTd.innerText = el.name;
+
+      const linkElement = document.createElement("a");
+      linkElement.href = '#';
+      linkElement.innerText = el.name;
+      linkElement.onclick = function () {
+        chrome.tabs.create({ active: true, url: el.link });
+      }
+
+      const textnodeEpisodeTd = document.createElement("td");
+      textnodeEpisodeTd.innerText = el.lastEpisode;
+
+      textnodeNameTd.appendChild(linkElement);
+      tableElementTr.appendChild(textnodeNameTd);
+      tableElementTr.appendChild(textnodeEpisodeTd);
+
+      document.getElementById("seriesTable").appendChild(tableElementTr);
+
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", el.link, true);
+      xhr.responseType = 'document';
+      xhr.onload  = function () {
+
+        console.log(el.name);
+        
+      }
+      xhr.send();
     }
 
-    var textnodeEpisodeTd = document.createElement("td");
-    textnodeEpisodeTd.innerText = el.lastEpisode;
-
-    textnodeNameTd.appendChild(linkElement);
-    tableElementTr.appendChild(textnodeNameTd);
-    tableElementTr.appendChild(textnodeEpisodeTd);
-
-    document.getElementById("seriesTable").appendChild(tableElementTr);
-    }
-  }
 });
+
+
