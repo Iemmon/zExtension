0e29dcf0663eb2a6bbcbb02ee5066a4e8d1b8dca
Iemmon <katerynab.ko@gmail.com>
2019-10-12 17:38:36 +0300

    update function added

--- pluginUtilities.js
+++ pluginUtilities.js
@@ -1,4 +1,4 @@
-function getSeriesFromDOM(document){
+function getSeriesFromDOM(document) {
     let seriesName = document.querySelector("h1 span[itemprop^='name']");
     if (seriesName == null) return;
     seriesName = seriesName.innerText;
@@ -9,7 +9,7 @@ function getSeriesFromDOM(document){
     let arrayOfEpisodes = new Array();
     const listOfEpisodes = document.querySelectorAll("div.season-list tr");
     for (let el of listOfEpisodes) {
-    
+
         const episodeData = el.querySelectorAll("td");
         let episode = {
             episodeNum: episodeData[1].innerText,
@@ -25,7 +25,7 @@ function getSeriesFromDOM(document){
                 newEpisode: false,
                 link: link
             };
-            console.log(seriesObject);
+
             break;
         }
         // 0 - season
@@ -34,6 +34,38 @@ function getSeriesFromDOM(document){
         // 3 - date
         // 4 - days left
     }
-
+    console.log("current episode");
+    console.log(seriesObject);
     return seriesObject;
 };
+
+function makeAMess(){
+
+    chrome.storage.sync.get({ 'series': [] }, function ({ series: result }) {
+        for (let el of result) {
+            el.lastEpisode = "3";
+        }
+        chrome.storage.sync.set({ 'series': result }, function () {
+            console.log("array updated");
+        });
+    });
+}
+
+function updateSeries(updatedSeries) {
+
+    chrome.storage.sync.get({ 'series': [] }, function ({ series: result }) {
+
+        for (let elFromStorage of result) {
+             //console.log(el);
+             //console.log(updatedSeries);
+            if (elFromStorage.name == updatedSeries.name) {
+                console.log(elFromStorage);
+                console.log(elFromStorage.lastEpisode + " changed to " + updatedSeries.lastEpisode);
+                elFromStorage.lastEpisode = updatedSeries.lastEpisode;
+                chrome.storage.sync.set({ 'series': result }, function () {
+                    //console.log("array updated");
+                });
+            }
+        }
+    });
+}
