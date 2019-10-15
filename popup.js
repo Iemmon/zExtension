e1ab8b4919d8f3d81054f8b4c186d5776eaab950
Iemmon <katerynab.ko@gmail.com>
2019-10-15 17:23:13 +0300

    remove button added to popup window

--- popup.js
+++ popup.js
@@ -10,46 +10,70 @@ chrome.storage.sync.get('color', function(data) {
   changeColor.setAttribute('value', data.color);
 });
 */
-chrome.storage.sync.get({ 'series': [] }, function ({ series: result }) {
-  console.log(result);
-  //makeAMess();
-    for (let el of result) {
+const draw = function () {
+	chrome.storage.sync.get({ 'series': [] }, function ({ series: result }) {
+		console.log(result);
+		// makeAMess();
+		document.getElementById("seriesTable").innerHTML = '';
 
-      const tableElementTr = document.createElement("TR");
+		for (let el of result) {
 
-      const textnodeNameTd = document.createElement("td");
-      // textnodeNameTd.innerText = el.name;
+			const tableElementTr = document.createElement("TR");
 
-      const linkElement = document.createElement("a");
-      linkElement.href = '#';
-      linkElement.innerText = el.name;
-      linkElement.onclick = function () {
-        chrome.tabs.create({ active: true, url: el.link });
-      }
+			const textnodeNameTd = document.createElement("td");
+			// textnodeNameTd.innerText = el.name;
 
-      const textnodeEpisodeTd = document.createElement("td");
-      textnodeEpisodeTd.innerText = el.lastEpisode;
+			const linkElement = document.createElement("a");
+			linkElement.href = '#';
+			linkElement.innerText = el.name;
+			linkElement.onclick = function () {
+				chrome.tabs.create({ active: true, url: el.link });
+			}
 
-      textnodeNameTd.appendChild(linkElement);
-      tableElementTr.appendChild(textnodeNameTd);
-      tableElementTr.appendChild(textnodeEpisodeTd);
+			const textnodeEpisodeTd = document.createElement("td");
+			textnodeEpisodeTd.innerText = el.lastEpisode;
 
-      document.getElementById("seriesTable").appendChild(tableElementTr);
+			const removeButton = document.createElement("button");
+			removeButton.innerText = "remove";
 
-      const xhr = new XMLHttpRequest();
-      xhr.open("GET", el.link, true);
-      xhr.responseType = 'document';
-      
-      xhr.onload  = function () {
-        const currentSeries = getSeriesFromDOM(xhr.response);
-        
-        updateSeries(currentSeries);
-        console.log(el.name);
-        
-      }
-      xhr.send();
-    }
+			textnodeNameTd.appendChild(linkElement);
+			tableElementTr.appendChild(textnodeNameTd);
+			tableElementTr.appendChild(textnodeEpisodeTd);
+			tableElementTr.appendChild(removeButton);
 
-});
+			document.getElementById("seriesTable").appendChild(tableElementTr);
+
+			removeButton.onclick = (function (element) {
+				return function () {
+					chrome.storage.sync.get({ 'series': [] }, function ({ series: result }) {
+						for (let i = 0; i < result.length; i++) {
+							if (result[i].name == element) {
+								result.splice(i, 1);
+								chrome.storage.sync.set({ 'series': result }, function () {
+									console.log("element removed with button");
+								});
+								draw();
+								break;
+							}
+						}
+					});
+				}
+			})(el.name);
+
+			const xhr = new XMLHttpRequest();
+			xhr.open("GET", el.link, true);
+			xhr.responseType = 'document';
+
+			xhr.onload = function () {
+				const currentSeries = getSeriesFromDOM(xhr.response);
+				updateSeries(currentSeries);
+				console.log(el.name);
+			}
+			xhr.send();
+		}
+
+	});
+}
 
 
+draw();
